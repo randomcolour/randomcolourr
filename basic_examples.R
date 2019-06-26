7118c05dced1f2cf1fdc218e657a7a2736ff6f0f
Matt Malin <matthewmalin@gmail.com>
2019-06-26 23:47:05 +0100

    Adding first test cases for randomcolour()

--- basic_examples.R
+++ basic_examples.R
@@ -7,7 +7,12 @@ randomcolour <- function()
 {
   hex_value <- as.character.hexmode(sample(2^24, 1) - 1, 1)
   
-  hex_colour <- paste0("#", paste0(rep("0", times = 6 - nchar(hex_value)), collapse = ""), hex_value)
+  hex_colour <- paste0(
+    "#", 
+    paste0(rep("0", times = 6 - nchar(hex_value)), collapse = ""), 
+    toupper(hex_value))
+  
+  return(hex_colour)
 }
 # test cases need to demonstrate padding works e.g. set seed 97 and first call would do it
 # as gives a87af for first sample so needs padding
@@ -16,7 +21,6 @@ colour_jpg <- function(colour, filename, width = 200, height = 200, ...) {
   jpeg(filename, bg = colour, width = width, height = height, ...)
   plot.new()
   dev.off()
-  
 }
 
 colour_plot <- function(colour, ...) {
@@ -28,12 +32,17 @@ randomcolour_plot <- function(...)
 {
   colour <- randomcolour()
   colour_plot(colour, ...)
+  
+  return(colour)
 }
 
 randomcolour_jpg <- function(filename, width = 200, height = 200, ...) 
 {
-  randomcolour() %>% 
-    colour_jpg(filename, width = width, height = height, ...)
+  colour <- randomcolour() 
+  
+  colour_jpg(colour, filename, width = width, height = height, ...)
+  
+  return(colour)
 }
 
 
---
+++ basic_test_examples.R
@@ -0,0 +1,24 @@
+library(testthat)
+
+test_that("random colours generated", {
+  expect_equal({
+    set.seed(1)
+    randomcolour()
+  }, 
+  "#43F860",
+  "non-padding example")
+  
+  expect_equal({
+    set.seed(97)
+    randomcolour()
+  }, 
+  "#0A87AF",
+  "padding example")
+  
+  expect_false(
+    {
+      set.seed(1)
+      randomcolour() == randomcolour()
+    },
+    "non-repetition")
+})
