903ec08e1c19692a6225b666f72d0c067ee87888
Matt Malin <matthewmalin@gmail.com>
2019-06-27 18:40:14 +0100

    changing to Super-Duper RNG to enure cross-R version compatibility

--- randomcolour/tests/testthat/test-randomcolour.R
+++ randomcolour/tests/testthat/test-randomcolour.R
@@ -1,17 +1,20 @@
+# Note below that RNG method specified away from default and
+# non-Mersenne Twister for reproducibility aross R versions,
+# due to Mersenne-Twister changing to use an updated version
 
 test_that("random colours generated", {
   expect_equal({
-    set.seed(1, kind = "Mersenne-Twister", normal.kind = "Inversion")
+    set.seed(1, kind = "Super-Duper")
     randomcolour()
   },
-  "#43F860",
+  "#147A9D",
   label = "non-padding example")
 
   expect_equal({
-    set.seed(97, kind = "Mersenne-Twister", normal.kind = "Inversion")
+    set.seed(98, kind = "Super-Duper") # known to give a hex code needing padding
     randomcolour()
   },
-  "#0A87AF",
+  "#0CCCC8",
   label = "padding example")
 
   expect_false(
