e8e0fcae2828e43873ecafeb58c57f9f0e275643
Matt Malin <matthewmalin@gmail.com>
2019-06-27 18:20:35 +0100

    setting up test structure for package

--- randomcolour/tests/testthat.R
+++ randomcolour/tests/testthat.R
@@ -1,20 +1,23 @@
 library(testthat)
+library(randomcolour)
+
+test_check("randomcolour")
 
 test_that("random colours generated", {
   expect_equal({
     set.seed(1)
     randomcolour()
-  }, 
+  },
   "#43F860",
   "non-padding example")
-  
+
   expect_equal({
     set.seed(97)
     randomcolour()
-  }, 
+  },
   "#0A87AF",
   "padding example")
-  
+
   expect_false(
     {
       set.seed(1)
