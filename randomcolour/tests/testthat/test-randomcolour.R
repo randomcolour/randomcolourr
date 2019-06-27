458f48c0a158cce73b37e368f85bf146ce796031
Matt Malin <matthewmalin@gmail.com>
2019-06-27 18:32:20 +0100

    adding RNG details to set.seed in tests for cross-R compatibility

--- randomcolour/tests/testthat/test-randomcolour.R
+++ randomcolour/tests/testthat/test-randomcolour.R
@@ -1,14 +1,14 @@
 
 test_that("random colours generated", {
   expect_equal({
-    set.seed(1)
+    set.seed(1, kind = "Mersenne-Twister", normal.kind = "Inversion")
     randomcolour()
   },
   "#43F860",
   label = "non-padding example")
 
   expect_equal({
-    set.seed(97)
+    set.seed(97, kind = "Mersenne-Twister", normal.kind = "Inversion")
     randomcolour()
   },
   "#0A87AF",
