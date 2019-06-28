5b40d4f4af9cbe8b251bcfc1f791310e36f5940c
Matt Malin <matthewmalin@gmail.com>
2019-06-28 21:38:33 +0100

    minor changes to message

--- randomcolour/R/console-colours.R
+++ randomcolour/R/console-colours.R
@@ -4,16 +4,14 @@
 # TODO: add to dependencies appropriately:
 library(crayon)
 
-randomcolour_message <- function(...) {
+randomcolour_message <- function(msg) {
   colour <- randomcolour()
   coloured_message <- make_style(colour)
 
-  cat(coloured_message(...))
+  cat(coloured_message(paste0(msg)))
 }
 
 message_rc <- randomcolour_message
-
-# randomcolour_message("hello randomcolour world")
 
 
 
